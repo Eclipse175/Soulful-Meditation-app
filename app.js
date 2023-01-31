9febec9d35c9070ce3b8f7386010ec95e598a9b6
Eclipse175 <fahimkhobar2004@gmail.com>
2023-01-31 16:17:32 +0300

    styles

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu');
+const logo = document.querySelector('h1')
+
+menu.addEventListener('click', function() {
+    document.classList.add('navigation-menu')
+})
