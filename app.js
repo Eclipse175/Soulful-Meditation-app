3762736e8a0d4cdfa6c7ef67eacf91640cc5d91b
Eclipse175 <fahimkhobar2004@gmail.com>
2023-02-01 12:42:02 +0300

    added an animated nav-bar

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const menu = document.getElementById('menu');
 const logo = document.querySelector('h1')
+const navMenu = document.getElementById('navigation-menu')
+const nav = document.querySelector('nav');
+const cross = document.getElementById('close');
 
 menu.addEventListener('click', function() {
-    document.classList.add('navigation-menu')
+    navMenu.style.width = '350px';
+    navMenu.style.display = 'flex';
+    //pushes content to the side
+    nav.style.marginLeft = '350px'
+})
+
+cross.addEventListener('click', function() {
+    navMenu.style.width = '0px';
+    navMenu.style.display = 'none';
+    //pushes content to the side
+    nav.style.marginLeft = '0px'
 })
