4a31d3e639d84453020870740f830a2c1fbf5f20
Eclipse175 <fahimkhobar2004@gmail.com>
2023-02-17 19:44:34 +0300

    added sad music and fixed ALL bugs

--- app.js
+++ app.js
@@ -4,46 +4,94 @@ const navMenu = document.getElementById('navigation-menu')
 const nav = document.querySelector('nav');
 const cross = document.getElementById('close');
 
-const chillVideo = document.getElementById('myVideo');
-const studyVideo = document.getElementById('studyM');
-
-
-const chillAudio = new Audio('rainsound.mp3');
-const studyAudio = new Audio('studymoosic.mp3');
 
 const body = document.querySelector('body');
-const timerButtons = document.getElementById('timer');
 //button selectors below :
-const studyMusic = document.getElementById('study');
+const studyMusic = document.getElementById('study')
 const meditateMusic = document.getElementById('meditate')
 const chillMusic = document.getElementById('chill')
-const sadMusic = document.getElementById('sad-hours')
-const Links = document.querySelectorAll('li')
+const sadMusic = document.getElementById('sad-as-heck')
+const Links = document.getElementsByTagName('li')
+
+const studyVideo = document.getElementById('studyM');
+const studyAudio = new Audio('studymoosic.mp3');
+
+const chillVideo = document.getElementById('myVideo');
+const chillAudio = new Audio('rainsound.mp3');
+
+const sadVideo = document.getElementById('sad-hours');
+const sadAudio = new Audio('sadsong.mp3');
 
 
 menu.addEventListener('click', function () {
     navMenu.style.width = '350px';
-    navMenu.style.display = 'flex';
     //pushes content to the side
     nav.style.marginLeft = '350px'
+    navMenu.style.display = 'flex';
 })
 
 cross.addEventListener('click', function () {
     navMenu.style.width = '0px';
-    navMenu.style.display = 'none';
     //pushes content to the side
-    nav.style.marginLeft = '0px'
-})
-
-chillMusic.addEventListener('click', function () {
-    chillVideo.style.display = 'block';
-    chillAudio.play();
-    timerButtons.style.display = 'flex';
+    nav.style.marginLeft = '0px';
+    navMenu.style.display = 'none';
 })
 
 studyMusic.addEventListener('click', function () {
-    studyAudio.play();
-    studyVideo.style.display = 'block';
-    timerButtons.style.display = 'flex';
+    if (studyMusic.click = true) {
+        studyVideo.play();
+        studyAudio.play();
+        studyVideo.style.display = 'block';
+
+        chillVideo.pause()
+        chillVideo.currentTime = 0;
+        chillAudio.pause();
+        chillAudio.currentTime = 0;
+        chillVideo.style.display = 'none';
+
+        sadVideo.pause()
+        sadVideo.currentTime = 0;
+        sadAudio.pause();
+        sadAudio.currentTime = 0;
+        sadVideo.style.display = 'none';
+    }
 })
 
+chillMusic.addEventListener('click', function () {
+    if (chillMusic.click = true) {
+
+        chillVideo.play();
+        chillAudio.play();
+        chillVideo.style.display = 'block';
+
+        studyVideo.pause()
+        studyVideo.currentTime = 0;
+        studyAudio.pause();
+        studyAudio.currentTime = 0;
+        studyVideo.style.display = 'none';
+
+        sadVideo.pause()
+        sadVideo.currentTime = 0;
+        sadAudio.pause();
+        sadAudio.currentTime = 0;
+        sadVideo.style.display = 'none';
+    }
+})
+
+sadMusic.addEventListener('click', function () {
+    sadVideo.play();
+    sadAudio.play();
+    sadVideo.style.display = 'block';
+
+    studyVideo.pause()
+    studyVideo.currentTime = 0;
+    studyAudio.pause();
+    studyAudio.currentTime = 0;
+    studyVideo.style.display = 'none';
+
+    chillVideo.pause()
+    chillVideo.currentTime = 0;
+    chillAudio.pause();
+    chillAudio.currentTime = 0;
+    chillVideo.style.display = 'none';
+})
